385672c83e94da5172559773b2b3eb708092ae8b
Larry Botha <larry@fixate.it>
2019-07-09 20:03:25 -0500

    update packages

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,4 @@
 module.exports = {
-  linters: {
-    // run doctoc only on changed markdown files
-    '**/*.md': ['doctoc --github', 'git add'],
-  },
+  // run doctoc only on changed markdown files
+  '**/*.md': ['doctoc --github', 'git add'],
 };
